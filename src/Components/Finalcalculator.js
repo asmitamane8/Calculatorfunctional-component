fee73878427a832ef31e2c8251d7b7ee82c3d0fa
asmitamane8 <asmita@synkrama.com>
2022-05-26 14:40:29 +0530

    commit

--- src/Components/Finalcalculator.js
+++ src/Components/Finalcalculator.js
@@ -19,19 +19,17 @@ function Finalcalculator() {
     const value = event.target.getAttribute("data-value");
     const ops = ["/", "*"];
     const oprator = ["+", "-", "/", "*"];
-    debugger;
     if (
       (ops.includes(value) && usersdata === " ") ||
       (oprator.includes(value) && oprator.includes(usersdata.slice(-1)))
     ) {
-      if (value == "+" || value == "-" || value == "*" || value == "/") {
+      if (value == "+" && value == "-") {
         let a = usersdata.slice(0, -1);
         setuserdata(a + value);
       } else {
         return;
       }
     } else {
-      debugger;
       switch (value) {
         case "clear":
           setuserdata(" ");
@@ -49,25 +47,25 @@ function Finalcalculator() {
     <div className="Calculator">
       <Display usersdata1={usersdata} />
       <Keypad>
-        <Button onClick={handleClick} label="C" value="clear" id="clr" />
+        <Button onClick={handleClick} label="C" value="clear" />
         <Button onClick={handleClick} label="7" value="7" />
         <Button onClick={handleClick} label="4" value="4" />
         <Button onClick={handleClick} label="1" value="1" />
         <Button onClick={handleClick} label="0" value="0" />
 
-        <Button onClick={handleClick} label="/" value="/" type="oprator" />
+        <Button onClick={handleClick} label="/" value="/" />
         <Button onClick={handleClick} label="8" value="8" />
         <Button onClick={handleClick} label="5" value="5" />
         <Button onClick={handleClick} label="2" value="2" />
         <Button onClick={handleClick} label="." value="." />
 
-        <Button onClick={handleClick} label="x" value="*" type="oprator" />
+        <Button onClick={handleClick} label="x" value="*" />
         <Button onClick={handleClick} label="9" value="9" />
         <Button onClick={handleClick} label="6" value="6" />
         <Button onClick={handleClick} label="3" value="3" />
         <Button onClick={handleClick} label="" value="null" />
 
-        <Button onClick={handleClick} label="-" value="-" type="oprator" />
+        <Button onClick={handleClick} label="-" value="-" />
         <Button
           onClick={handleClick}
           label="+"
@@ -86,4 +84,5 @@ function Finalcalculator() {
     </div>
   );
 }
+
 export default Finalcalculator;
